1bf0027f1925d147c5ba874e222c6f9c377d2651
New Age Coder <newagecoder00@gmail.com>
2023-12-30 15:10:37 +0530

    SForYou

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+// const ejsMate = require("ejs-mate");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+
+//Home Route
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+app.get("/home", (req, res) => {
+    res.render("index.ejs",);
+});
+app.get("/yes", (req, res) => {
+    res.render("yes.ejs",);
+});
+
+
+app.listen(5050, () => {
+    console.log("server is listening to port 5050");
+});
